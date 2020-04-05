16dcd53a1d334ccb14d70428d592426ec4c60d5a
DayanRojas <dayan18_13@hotmail.com>
2020-04-05 23:51:46 +0200

    player performing 4 movements (LRUD)

--- player.js
+++ player.js
@@ -6,27 +6,26 @@ class Player {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
 
-        this.spriteWidth = 137;
-        this.spriteHeight = 49;
+        this.spriteWidth = 132;
+        this.spriteHeight = 195;
 
 
         this.image = new Image();
-        this.image.src = "./img/zelda6.png";
-
-        let rows = 1;
-        let cols = 4;
+        this.image.src = "./img/zeldaPower.png";
 
         this.image.framesIndex = 0;
         this.image.frames = 4;
-        this.maxFrameLength = 2
+        this.image.rows = 4;
 
         this.width = 50
         this.height = 50
 
+        this.status = ["POWER", "NORMAL"]
+        this.actualStatus = this.status[0];
+
         this.posX = 400;
         this.posY = 200;
         this.posY0 = this.posY;
-
 
         this.keys = keys;
 
@@ -40,21 +39,26 @@ class Player {
 
     draw(framesCounter) {
         const frameX = this.image.framesIndex;
-        const frameY = 0;
+        const frameY = this.image.posY;
         const sx = frameX * Math.floor(this.spriteWidth / this.image.frames);
-        const sy = frameY;
+        const sy = frameY * Math.floor(this.spriteHeight / this.image.rows);
 
-        // if (this.image.frames > this.maxFrameLength) this.image.frames = 0;
-        this.ctx.drawImage(this.image, sx, sy, Math.floor(this.image.width / this.image.frames), this.image.height, this.posX, this.posY, this.width, this.height);
+        console.log('width', sx)
+        console.log('height', sy)
+
+
+        this.ctx.drawImage(this.image, sx, sy, Math.floor(this.image.width / this.image.frames), this.height, this.posX, this.posY, this.width, this.height);
         //this.bullets.forEach(bullet => bullet.draw())
         //this.clearBullets()
         //this.move()
 
     }
 
-    animate(pos) {
+    animate(pos, spriteIndexs) {
         this.image.posY = pos[1];
-        this.image.framesIndex = ++this.image.framesIndex % (this.image.frames / 2);
+        let max = spriteIndexs.length
+        this.image.framesIndex = (++this.image.framesIndex % max);
+        console.log('INDEX', this.image.framesIndex)
     }
 
     move(dir) {
@@ -69,19 +73,19 @@ class Player {
             switch (e.keyCode) {
                 case this.keys.TOP:
                     this.move('top')
-                    this.animate();
+                    this.animate([0, 3], [0, 1, 2, 3]);
                     break;
                 case this.keys.LEFT:
                     this.move('left')
-                    this.animate([0, 1]);
+                    this.animate([0, 1], [0, 1, 2, 3]);
                     break;
                 case this.keys.RIGHT:
                     this.move('right')
-                    this.animate();
+                    this.animate([0, 2], [0, 1, 2, 3]);
                     break;
                 case this.keys.DOWN:
                     this.move('down')
-                    this.animate();
+                    this.animate([0, 0], [0, 1, 2, 3]);
                     break;
                 case this.keys.SPACE:
                     this.shoot();
